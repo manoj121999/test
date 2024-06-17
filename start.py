935cc94e4c812ed00b38baa263d6526eea548cb2
Manoj Sai Charan Mangena <mmangena@nextsphere.com>
2024-06-17 19:48:56 +0530

    test123qqqqaaa

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 def add(x, y):
     print(x + y)
 if __name__ == "__main__":
-    x = int(input("Enter number Z :" ))
-    y = int(input("number y :" ))
+    x = int(input("Enter number l :" ))
+    y = int(input("Enter number y :" ))
     print(add(x, y))
