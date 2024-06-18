3b1299376197c69ba0a6bd6a67e67071d268d5ba
Manoj Mangena <70632603+manoj121999@users.noreply.github.com>
2024-06-18 06:36:49 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,5 +2,5 @@ def add(x, y):
     print(x + y)
 if __name__ == "__main__":
     x = int(input("Enter number l :" ))
-    y = int(input("Enter number y :" ))
+    y = int(input("Enter number k :" ))
     print(add(x, y))
